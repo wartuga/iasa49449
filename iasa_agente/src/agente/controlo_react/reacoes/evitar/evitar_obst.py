c6274e91ab4da5946859daa24a9253182b3fd100
Diogo Almeida <77116842+wartuga@users.noreply.github.com>
2024-04-21 16:23:17 +0100

    added documentation & Solucao done

--- iasa_agente/src/agente/controlo_react/reacoes/evitar/evitar_obst.py
+++ iasa_agente/src/agente/controlo_react/reacoes/evitar/evitar_obst.py
@@ -4,7 +4,44 @@ from agente.controlo_react.reacoes.evitar.evitar_dir import EvitarDir
 from sae.ambiente.direccao import Direccao
 
 """
-// meter comentários sobre os apontamentos da aula
+Por possuir memória podemos ter comportamentos como o de "evitar o passado"
+Neste tipo de comportamento, a reação não depende apenas da percepçáo, mas
+também da memória.
+
+Para isto é necessário ter manutenção de estado.
+As vantagens são: 
+    - ter a capacidade de produzir todo o tipo de comportamento
+    - ter evoluçáo de estado ao longo do tempo
+    - poder agir devido à ausência de memória
+    - suporta situações de falha por meio da exploração de ações não 
+    realizadas anteriormente
+
+As desvantagens são:
+    - necessidade de memória => necessidade de mais recursos
+    - necessidade de manter as representações de estado => aumento de complexidade
+
+Arquitetura de Subsunção
+
+Resultado do comportamento pode ser a entrada de outro comportamento
+
+Camadas superiores controlam as camadas inferiores => Hierarquia de camadas
+
+Inibição
+	Inibir as entradas
+Supressão
+	Suprimir as saídas
+Reinício 
+	Reset do estado interno
+	
+
+Definida por conjuntos de comporamentos
+Comportamentos organizados em camadas (niveis de competência)
+Desenvolvimento incremental
+Robustas
+Relativamente simples com um problema
+	Facilmente se transforma numa teia (se for muito complexa)
+
+Niveis de competência não mantém memória, os comportamentos sim
 """
 """
 Representa a reação de evitar um obstaculo
--- iasa_agente/src/lib/pee/mec_proc/solucao.py
+++ iasa_agente/src/lib/pee/mec_proc/solucao.py
@@ -1,11 +1,37 @@
-
+from pee.mec_proc.passo_solucao import PassoSolucao
+from collections import deque 
 """
-exercício para casa
-fazer solução
+Representa a solução de um problema.
+Tem como propriedades o custo total e a dimensão da solução.
+Possui ainda os passos, que é um PassoSolucao para cada nó da soluçáo,
+é iterável.
 """ 
 class Solucao:
 
     def __init__(self, no_final):
-        raise NotImplementedError
-
+        self.__custo = no_final.custo
+        self.__dimensao = no_final.profundidade
+        # uma doubly ended queue(dequeue) is preferred over a list in this case
+        # since we are walking through the nodes and starting in the last one
+        # with a deque we have a O(1) complexity in append and pop operations
+        # we dont need to do: list.reverse() since we can appendLeft()
+        self.__passos = deque([])
+        no = no_final
+        while no.antecessor:
+            self.__passos.appendleft(PassoSolucao(no.estado, no.operador))
+            no = no.antecessor
+        self.__passos.appendleft(PassoSolucao(no.estado, no.operador))
     
+    @property
+    def custo(self):
+        return self.__custo
+    
+    @property
+    def dimensao(self):
+        return self.__dimensao
+    
+    def __iter__(self):
+        iter(self.__passos)
+
+    def __getitem__(self, index):
+        return self.__passos[index]
